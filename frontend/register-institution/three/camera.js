3f4e7f364c7eb212d8fd62b7b7fb9742797d97bc
Dilip Kumar Meena <164808784+buildcampusone@users.noreply.github.com>
2026-06-26 20:38:15 +0530

    Initialize PerspectiveCamera in camera.js

--- frontend/register-institution/three/camera.js
+++ frontend/register-institution/three/camera.js
@@ -1,1 +1,17 @@
+import * as THREE from "three";
 
+export const camera=
+new THREE.PerspectiveCamera(
+
+60,
+
+window.innerWidth/
+window.innerHeight,
+
+0.1,
+
+1000
+
+);
+
+camera.position.set(0,2,12);

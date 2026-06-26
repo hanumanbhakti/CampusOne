4440ddcae3d8d4d53088f2ab56662d36baf7dc32
Dilip Kumar Meena <164808784+buildcampusone@users.noreply.github.com>
2026-06-26 20:38:47 +0530

    Initialize WebGLRenderer with antialias and alpha

--- frontend/register-institution/three/renderer.js
+++ frontend/register-institution/three/renderer.js
@@ -1,1 +1,24 @@
+import * as THREE from "three";
 
+export const renderer=
+new THREE.WebGLRenderer({
+
+antialias:true,
+
+alpha:true
+
+});
+
+renderer.setPixelRatio(
+Math.min(
+window.devicePixelRatio,
+2
+));
+
+renderer.setSize(
+
+window.innerWidth,
+
+window.innerHeight
+
+);

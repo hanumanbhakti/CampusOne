40695baa924584d807859c7dca59ca401313e507
Dilip Kumar Meena <164808784+buildcampusone@users.noreply.github.com>
2026-06-26 20:39:31 +0530

    Add ambient and point lights to the scene

--- frontend/register-institution/three/lights.js
+++ frontend/register-institution/three/lights.js
@@ -1,1 +1,25 @@
+import * as THREE from "three";
 
+import {scene} from "./scene.js";
+
+const ambient=
+new THREE.AmbientLight(
+0xffffff,
+1.5
+);
+
+scene.add(ambient);
+
+const point=
+new THREE.PointLight(
+0x2563EB,
+15
+);
+
+point.position.set(
+5,
+8,
+5
+);
+
+scene.add(point);
